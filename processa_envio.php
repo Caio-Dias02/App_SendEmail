96715e29e7efc4c5d97fcfffe35a1d38f6f937ca
Caio-Dias02 <you@example.com>
2021-12-10 15:54:33 -0300

    Created Class Mensagem

--- processa_envio.php
+++ processa_envio.php
@@ -1,2 +1,47 @@
 <?php
-    print_r($_POST);
+// print_r($_POST);
+
+class Mensagem
+{
+    private $para = null;
+    private $assunto = null;
+    private $mensagem = null;
+
+
+
+    public function __set($atributo, $valor)
+    {
+        $this->$atributo = $valor;
+    }
+
+    public function __get($atributo)
+    {
+        return $this->$atributo;
+    }
+
+    public function mensagemValida()
+    {
+        if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+          return false;
+        } else{
+            return true;
+        }
+
+
+    }
+}
+
+$mensagem = new Mensagem();
+
+$mensagem->__set('para', $_POST['para']);
+$mensagem->__set('assunto', $_POST['assunto']);
+$mensagem->__set('mensagem', $_POST['mensagem']);
+
+// print_r($mensagem);
+
+if($mensagem->mensagemValida()){
+    echo 'verdade';
+} else{
+    echo 'falso';
+}
+
